5f11a0083e7a72609e594a5d4e024e91e4182fae
Phoenix616 <max@themoep.de>
2022-09-29 12:29:04 +0100

    Change copyright to GPLv3 and include license file
    Also ignore dependency-reduced-pom

--- bukkit/src/main/java/de/themoep/dynamicslots/bukkit/DynamicSlotsCommand.java
+++ bukkit/src/main/java/de/themoep/dynamicslots/bukkit/DynamicSlotsCommand.java
@@ -1,26 +1,29 @@
 package de.themoep.dynamicslots.bukkit;
 
+/*
+ * DynamicSlots - bukkit
+ * Copyright (C) 2022. Max Lee aka Phoenix616 (max@themoep.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import de.themoep.dynamicslots.core.DynamicSlotsCommandLogic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-/*
- * Copyright 2017 Max Lee (https://github.com/Phoenix616/)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Mozilla Public License as published by
- * the Mozilla Foundation, version 2.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Mozilla Public License v2.0 for more details.
- *
- * You should have received a copy of the Mozilla Public License v2.0
- * along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
- */
 public class DynamicSlotsCommand implements CommandExecutor {
     private final DynamicSlots plugin;
 
